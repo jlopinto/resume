d41b83f525f10e258c11ef0ab381153ef6a9ac3e
jlopinto <julien.lopinto@gmail.com>
2019-10-30 17:47:30 +0100

    Fixing watch task

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,8 +1,8 @@
 import gulp from 'gulp';
 import conf from './helpers/options';
 
-function watchTask() {
-  gulp.watch(conf.paths.src.images, gulp.series('image'));
+function watchTask () {
+  gulp.watch(conf.paths.src.images, gulp.series('images'));
   gulp.watch(`${conf.paths.src.scripts}**/*.js`, gulp.series('scripts'));
   gulp.watch(conf.paths.src.styles, gulp.series('styles'));
   gulp.watch(conf.paths.src.fonts, gulp.series('copy:fonts'));
